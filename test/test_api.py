617b9447661f7e4863b9a5fe56158c80c3b99498
joaaax <joaax@computas.com>
2020-06-24 14:24:56 +0200

    Add example test of root

--- test/test_api.py
+++ test/test_api.py
@@ -12,3 +12,10 @@ def client():
     api.app.config['TESTING'] = True
     with api.app.test_client() as client:
         yield client
+
+def test_root_example(client):
+    """
+        Use GET request on root and check if the response is correct.
+    """
+    req = client.get('/')
+    assert(req.data == b'Hello, World!')
