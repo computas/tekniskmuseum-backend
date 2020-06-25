9746cee53cf02222b0ed866c1e0e30d8c3077df6
joaaax <joaax@computas.com>
2020-06-25 08:54:25 +0200

    Define test functions for allowedFile function

--- test/test_api.py
+++ test/test_api.py
@@ -1,8 +1,9 @@
 import sys
+import werkzeug
 import pytest
-import api
 
 sys.path.insert(1, "./flask")
+import api
 
 
 @pytest.fixture
@@ -19,5 +20,37 @@ def test_root_example(client):
     """
         Use GET request on root and check if the response is correct.
     """
-    req = client.get("/")
-    assert req.data == b"Hello, World!"
+    req = client.get('/')
+    assert(req.data == b'Hello, World!')
+
+
+def test_allowedFile_small_resolution(client):
+    """
+        Test if the allowedFile function within the API returns False if an
+        image with too small resolution is sent as parameter.
+    """
+    pass
+
+
+def test_allowedFile_too_large_file(client):
+    """
+        Test if the allowedFile function within the API returns False if an
+        image above the maximum size is sent as parameter.
+    """
+    pass
+
+
+def test_allowedFile_wrong_format(client):
+    """
+        Test if the allowedFile function within the API returns False if an
+        image of the wrong format is sent as parameter.
+    """
+    pass
+
+
+def test_allowedFile_correct(client):
+    """
+        Test if the allowedFile function within the API returns True if an
+        image with all constraints satisfied is sent as parameter.
+    """
+    pass
