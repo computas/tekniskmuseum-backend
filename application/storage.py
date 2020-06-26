1543faefffe09bceccdfafacd2df1e8d78a1330a
Marius Brateng <mahbx@computas.com>
2020-06-26 11:00:52 +0200

    Refactor api.py reading config.json

--- application/storage.py
+++ application/storage.py
@@ -1,8 +1,12 @@
 """
 Tools for interacting with Azure blob storage.
 """
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import uuid
-import secrets
+import keys
 from azure.storage.blob import BlobClient
 
 
