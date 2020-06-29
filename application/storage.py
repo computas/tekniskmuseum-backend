12e0aed17aecb9372387daf8922660d6b9221ec1
johncx <johnx@computas.com>
2020-06-29 10:04:00 +0200

    storage now stores new drawings separate from original dataset

--- application/storage.py
+++ application/storage.py
@@ -1,25 +1,30 @@
 """
 Tools for interacting with Azure blob storage.
 """
+import os
 import uuid
 import secrets
 from azure.storage.blob import BlobClient
+import pdb
 
 
 def saveImage(image, label):
     """
     Upload image to blob storage container with same name as image label.
-    Image is renamed to assure unique name.
+    Image is renamed to assure unique name. Returns public URL to access image
     """
     filename = label + uuid.uuid4().hex + ".png"
     connectionString = secrets.get("BLOB_CONNECTION_STRING")
+    containerName = 'new-' + label 
+    baseurl = secrets.get('BASE_IMAGE_URL')
     try:
         blob = BlobClient.from_connection_string(
             conn_str=connectionString,
-            container_name=label,
-            blob_name=filename,
-        )
+            container_name=containerName,
+            blob_name=filename)
         blob.upload_blob(image)
     except Exception as e:
         print(e)
-    return
+    url = baseurl + '/' + containerName + '/' + filename
+    pdb.set_trace()
+    return url
