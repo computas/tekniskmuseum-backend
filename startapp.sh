703dea9d655fac34d3680291c4e0719296a15347
johncx <johnx@computas.com>
2020-07-01 16:31:10 +0200

    added testrunnig to startapp script

--- startapp.sh
+++ startapp.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+# Start webapp or run tests
+
+# compute number of gunicorn workers
 NCORES=$(nproc)
 NWORKERS=$(((2*$NCORES)+1))
 if [[ $NWORKERS > 12 ]]; then
     NWORKERS=12
 fi
+
+# check if test run
+while [[ "$#" > 0 ]]; do
+    case $1 in
+        -t | --test) test=true; shift ;;
+        *) echo "Unexpected option: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+
+if [[ $test = true ]]; then
+    python -m pytest
+    exit
+fi
+
 
 echo $(which python)
 echo $(python --version)
