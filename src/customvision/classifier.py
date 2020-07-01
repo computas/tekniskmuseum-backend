763433c67ca3f2fedfcbbaed63648d4d3e3c4de6
Håkon Osland <hosx@computas.com>
2020-07-01 15:37:03 +0200

    removed useless line from classifier

--- src/customvision/classifier.py
+++ src/customvision/classifier.py
@@ -25,7 +25,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utilities.keys import Keys  # noqa: e402
 
 
-
 class Classifier:
     def __init__(self) -> None:
         """
