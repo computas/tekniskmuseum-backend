cdcc1afcabbe1dc9071f7f0cb4299c1898844340
joaaax <joaax@computas.com>
2020-07-02 10:44:09 +0200

    Use configuration file, add function to construct path

--- src/test/test_api.py
+++ src/test/test_api.py
@@ -4,7 +4,7 @@ import werkzeug
 import tempfile
 import pytest
 from webapp import api
-
+from test import config as cfg
 
 @pytest.fixture
 def client():
@@ -31,7 +31,7 @@ def test_allowedFile_small_resolution():
     """
     # Test the allowedFile function with the given filename.
     # The allowedFile function should return 'false'.
-    allowed_file_helper("allowedFile_test1.png", False)
+    allowed_file_helper(cfg.api_image1, False)
 
 
 def test_allowedFile_too_large_file():
@@ -41,7 +41,7 @@ def test_allowedFile_too_large_file():
     """
     # Test the allowedFile function with the given filename.
     # The allowedFile function should return 'false'.
-    allowed_file_helper("allowedFile_test2.png", False)
+    allowed_file_helper(cfg.api_image2, False)
 
 
 def test_allowedFile_wrong_format():
@@ -51,7 +51,7 @@ def test_allowedFile_wrong_format():
     """
     # Test the allowedFile function with the given filename.
     # The allowedFile function should return 'false'.
-    allowed_file_helper("allowedFile_test3.jpg", False)
+    allowed_file_helper(cfg.api_image3, False)
 
 
 def test_allowedFile_correct():
@@ -61,15 +61,17 @@ def test_allowedFile_correct():
     """
     # Test the allowedFile function with the given filename.
     # The allowedFile function should return 'true'.
-    allowed_file_helper("allowedFile_test4.png", True)
+    allowed_file_helper(cfg.api_image4, True)
 
 
 def allowed_file_helper(filename, expected_result):
     """
         Helper function for the allowedFile function tests.
     """
-    # The path is only valid if the program runs from the outmost directory
-    path = os.path.join("..", "data", filename)
+    # Construct path to the directory with the images
+    dir_path = construct_path(cfg.api_path_data)
+    # The path is only valid if the program runs from the src directory
+    path = os.path.join(dir_path, filename)
     with open(path, "rb") as f:
         data_stream = f.read()
         # Create temporary file and reset seek to avoid EOF errors
@@ -82,3 +84,17 @@ def allowed_file_helper(filename, expected_result):
         result = api.allowed_file(image)
 
     assert result == expected_result
+
+def construct_path(dir_list):
+    """
+        Take in a list of directories in sequential order with regards to path
+        order and construct a relative path to the last directory in the list.
+    """
+    # Add first element to path
+    path = dir_list[0]
+    # Iterate over the list (exclude 1st element)
+    for elem in dir_list[1:]:
+        # Append the remaining paths sequentially
+        path = os.path.join(path, elem)
+    
+    return path
