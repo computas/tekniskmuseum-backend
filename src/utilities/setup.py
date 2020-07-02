432ae3b3f31c6ceec4d886c75a196b37db82935f
Håkon Osland <hosx@computas.com>
2020-07-02 16:26:02 +0200

    remove stuff from setup.py

--- src/utilities/setup.py
+++ src/utilities/setup.py
@@ -2,14 +2,3 @@
 CV_MAX_ITERATIONS = 10
 # can't upload more than 64 images at a time, if more
 CV_MAX_IMAGES = 64
-labels = [
-    "ambulance",
-    "bench",
-    "circle",
-    "drawings",
-    "square",
-    "star",
-    "sun",
-    "triangle",
-]
-time_limit = 22
