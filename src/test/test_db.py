bb856a4f00887aacc3258da73cfbde4a760249d0
andrealouisestanderen <anstx@computas.com>
2020-07-02 10:38:48 +0200

    Modified test_db.py to use a separate testing database

--- src/test/test_db.py
+++ src/test/test_db.py
@@ -1,1 +1,23 @@
-from 
+"""
+    Testfunctions for testing functions to manipualte the database. The
+    functions is used on a identical test database.
+"""
+
+from webapp import api
+from webapp import models
+
+
+def test_create_tables():
+    models.create_all
+
+
+def create_tables(app):
+    """
+        Something.
+    """
+    with api.app.app_context():
+        api.db.create_all()
+
+
+
+def test_insert_into_test_games():
