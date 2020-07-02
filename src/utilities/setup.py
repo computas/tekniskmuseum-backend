0adb29e73f533c9a3fbf868a1ecf9228fe684a6c
Håkon Osland <hosx@computas.com>
2020-07-02 11:42:51 +0200

    fix comment in setup.py

--- src/utilities/setup.py
+++ src/utilities/setup.py
@@ -1,6 +1,6 @@
 # custom vision can't have more than 10 iterations at a time, if more classifier.py will delete the oldest iteration
 CV_MAX_ITERATIONS = 10
-# can't upload more than 64 images at a time, if more
+# can't upload more than 64 images at a time, if more images are present, custom vision will return an error message
 CV_MAX_IMAGES = 64
 labels = [
     "ambulance",
