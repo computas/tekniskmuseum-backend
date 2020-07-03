bb17e25e2be35ff5d792d55b5acd7bc73089704e
Marius Brateng <mahbx@computas.com>
2020-07-03 10:27:07 +0200

    the commit now runs

--- src/customvision/classifier.py
+++ src/customvision/classifier.py
@@ -73,7 +73,7 @@ class Classifier:
 
         # find published iterations
         puplished_iterations = [
-            iteration for iteration in iterations if iteratoin.publish_name != None]
+            iteration for iteration in iterations if iteration.publish_name != None]
 
         # get the latest published iteration
         puplished_iterations.sort(key=lambda i: i.created)
