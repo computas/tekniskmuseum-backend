92dcf0fd84059f89940724de09de2190645dc8cd
andrealouisestanderen <anstx@computas.com>
2020-07-06 08:31:52 +0200

    Fix database test

--- src/test/test_db.py
+++ src/test/test_db.py
@@ -1,6 +1,6 @@
 """
     Testfunctions for testing functions to manipualte the database. The
-    functions is used on a identical test database.
+    functions is used on an identical test database.
 """
 
 from webapp import api
@@ -8,10 +8,8 @@ from webapp import models
 import uuid
 import time
 import pytest
+import pdb
 
-token = uuid.uuid4().hex
-start_time = time.time()
-label = "label"
 
 @pytest.fixture(scope="session")
 def client():
@@ -22,20 +20,21 @@ def client():
     with api.app.test_client() as client:
         yield client
 
-def test_create_tables():
-    
-    result = models.create_tables(api.app)
 
+def test_create_tables():
+    result = models.create_tables(api.app)
     assert result == "Models created!"
 
 @pytest.fixture(scope="session")
-def test_insert_into_test_games():
+def test_insert_into_games():
+    token = uuid.uuid4().hex
     start_time = time.time()
     result = models.insert_into_games(token, start_time, "sun")
+    breakpoint()
     assert result == "Inserted"
 
 @pytest.fixture(scope="session")
-def test_query_tables():
+def test_query_games():
    result = models.query_game(token)
    #if result not string
    assert type(result) == "list"
@@ -45,9 +44,6 @@ def test_query_euqals_insert():
     token2 = uuid.uuid4().hex
     start_time = time.time()
     models.insert_into_games(token2, start_time, "bench")
-    result = [start_time, "bench"]
-    expected_result = models.query_game(token2)
+    expected_result = [start_time, "bench"]
+    result = models.query_game(token2)
     assert result == expected_result
-
-    
-
