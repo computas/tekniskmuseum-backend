688b1055beb93d5fa7b259347f3bf71fc1827437
johancx <67272614+johancx@users.noreply.github.com>
2020-07-08 10:04:15 +0200

    Updated comment

--- startapp.sh
+++ startapp.sh
@@ -23,9 +23,9 @@ Options:
     -w, --workers   Specify number of gunicorn workers,
                     recommended values are 3-12 workers.'
 
-# print headline with first argument
+# print headline with text. Optional first argument determines color.
 printHeadline() {
-    # use green text if first argumnet is 'green'
+    # use green/red text if first argumnet is 'green' or 'red'
     if [[ $1 = green ]]; then
         printf '\e[32m'
         shift
@@ -85,7 +85,7 @@ $(which python)
 Number processing units: $ncores
 Number of workers: $nworkers"
 
-# Flask default_settings
+# Default settings and entry point to flask
 default_settings="--timeout=600 -w=$nworkers --chdir src/ webapp.api:app"
 logfile='/home/LogFiles/flaskapp.log'
 
