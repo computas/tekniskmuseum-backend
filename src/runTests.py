34e811400f67cc3c8777f7699279819424cd3902
Marius Brateng <mahbx@computas.com>
2020-07-09 16:45:20 +0200

    debugging

--- src/runTests.py
+++ src/runTests.py
@@ -1,19 +1,25 @@
 #!/bin/python3
 import json
 import os
+import sys
 import pytest
 import argparse
+import pdb
 
 # parse cli arguments
 parser = argparse.ArgumentParser(description='get key string')
 parser.add_argument('--keys', action='store', type=str,
                     help='The text to parse.')
-jsonstring = parser.parse_args()
+arguments = parser.parse_args()
+
 
 # export keys
-keylist = json.reads(jsonstring)
+keylist = json.loads(arguments.keys)
 for keydict in keylist:
-    os.environ[keydict.name] = keydict.key
+    os.environ[keydict["name"]] = keydict["value"]
+
+# clear cli arguments as pytest reads them
+sys.argv = [sys.argv[0]]
 
 # run pytets
 pytest.main()
