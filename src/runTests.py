ba0ddf75f0216ee1c8ba9e92335e316ab449f897
Marius Brateng <mahbx@computas.com>
2020-07-10 09:48:21 +0200

    testing in github actions now runs. add documentation

--- src/runTests.py
+++ src/runTests.py
@@ -1,4 +1,8 @@
 #!/bin/python3
+"""
+    This file is used for running pytests in github actions. It parses keys from github sectrets and runs tests. 
+    The file is called in github action with secrets as argument. 
+"""
 import json
 import os
 import sys
