4d964fc6161321653077dbc92251c4f186b76463
andreacx <67271710+andreacx@users.noreply.github.com>
2020-07-13 08:32:08 +0200

    Update api
    Change method for selecting 3 labels from choices to sample.

--- src/webapp/api.py
+++ src/webapp/api.py
@@ -58,7 +58,7 @@ def start_game():
     """
     # start a game and insert it into the games table
     token = uuid.uuid4().hex
-    labels_list = random.choices(labels, k=num_games)
+    labels_list = random.sample(labels, num_games)
     today = str(date.today())
     models.insert_into_games(token, json.dumps(labels_list), 0.0, today)
     # return game data as json object
