e4f6fefa4d4871a3e4c2a90a6becc4a94e7bc50a
Thomas Bakken Moe <thom@computas.com>
2024-06-17 15:58:10 +0200

    Adds Script for installing SQL driver on Ubuntu/WSL

---
+++ install-mysql-driver.sh
@@ -0,0 +1,22 @@
+# Installs MS ODBC 17 SQL Driver.
+# Intended to run on Ubuntu/WSL
+
+if ! [[ "16.04 18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
+then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
+    exit;
+fi
+
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+
+sudo curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
+
