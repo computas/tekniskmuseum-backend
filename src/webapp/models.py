48b3898491cd38caada5fcf692e49c02cf1b0d17
ComputasAlex <almax@computas.com>
2024-07-04 10:41:43 +0200

    format

--- src/webapp/models.py
+++ src/webapp/models.py
@@ -482,8 +482,11 @@ def get_n_labels(n, difficulty_id):
     try:
 
         # get labels from three most recent games
-        games = Games.query.filter(Games.difficulty_id <= difficulty_id, difficulty_id
-                                   - Games.difficulty_id < 2).order_by(Games.date.desc()).limit(3).all()
+        games = Games.query.filter(
+            Games.difficulty_id <= difficulty_id,
+            difficulty_id
+            - Games.difficulty_id < 2).order_by(
+            Games.date.desc()).limit(3).all()
         labels_to_filter = [
             label for game in games for label in json.loads(game.labels)]
 
@@ -494,7 +497,8 @@ def get_n_labels(n, difficulty_id):
 
         minimum_labels = 6
         for label in labels_to_filter:
-            if label in english_labels and len(english_labels) > minimum_labels:
+            if label in english_labels and len(
+                    english_labels) > minimum_labels:
                 english_labels.remove(label)
 
         random_list = random.sample(english_labels, n)
--- startapp.sh
+++ startapp.sh
@@ -2,7 +2,7 @@
 # This script serves as the main interface to the app
 
 # Compute number of gunicorn workers
-ncores=$(nproc)
+ncores=$(sysctl -n hw.logicalcpu)
 nworkers=$(((2*$ncores)+1))
 if [[ $nworkers -gt 12 ]]; then
     nworkers=12
