926ae76c214353d45b70fa6587a3bd5b2c03d709
Andreas Berg <aberx@computas.com>
2024-07-16 10:24:41 +0200

    Small script to get list of words to upload to customvision

---
+++ src/utilities/csv_to_list.py
@@ -0,0 +1,13 @@
+import csv
+
+# Take all words from first column of csv and print them as a list with " " around each word.
+# Open the CSV file
+with open('./dict_eng_to_nor_difficulties_v2.csv', 'r') as file:
+    # Create a CSV reader object
+    reader = csv.reader(file)
+    
+    # Extract the words from the first column and store them in a list
+    words = [row[0] for row in reader]
+    
+    # Print the words as a list to use in classifier.upload_images() function
+    print([f"{word}" for word in words])
