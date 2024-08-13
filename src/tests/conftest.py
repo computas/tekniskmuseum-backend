2460c2c8bd695b0e86dcc8aaa0dc6b0e1065041c
ivaj <ivaj@computas.com>
2024-08-13 12:44:47 +0200

    fix: applied linter

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -7,14 +7,12 @@ import os
 current_dir = os.path.dirname(__file__)
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.insert(0, project_root)
-
-
 from webapp.api import app
+
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-
     with app.test_client() as client:
         with app.app_context():
             pass
