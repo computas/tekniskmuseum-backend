65fa123c1dbe955b0961427fcefda0fa6338423d
ellenyuX <ellen.yu@computas.com>
2024-08-29 15:30:02 +0200

    Filtering on ERROR log entries

--- src/webapp/api.py
+++ src/webapp/api.py
@@ -417,7 +417,7 @@ def get_error_logs():
             match = re.match(log_pattern, line)
             if match:
                 log_dict = match.groupdict()
-                if log_dict["level"] == "INFO":
+                if log_dict["level"] == "ERROR":
                     data.append(log_dict)
     
         return json.dumps(data), 200
